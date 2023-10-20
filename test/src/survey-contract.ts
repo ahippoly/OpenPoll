116a951b31da24d8d3eaede7c5bac56e5a7fa519
Arnaud <arnaud.hippo@gmail.com>
2023-10-20 23:31:44 +0200

    Initialize subgraph

---
+++ test/src/survey-contract.ts
@@ -0,0 +1,39 @@
+import {
+  SurveyAnswered as SurveyAnsweredEvent,
+  SurveyPublished as SurveyPublishedEvent
+} from "../generated/SurveyContract/SurveyContract"
+import { SurveyAnswered, SurveyPublished } from "../generated/schema"
+
+export function handleSurveyAnswered(event: SurveyAnsweredEvent): void {
+  let entity = new SurveyAnswered(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.fileCID = event.params.fileCID
+  entity.answers = event.params.answers
+  entity.zkAnswers = event.params.zkAnswers
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleSurveyPublished(event: SurveyPublishedEvent): void {
+  let entity = new SurveyPublished(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.name = event.params.name
+  entity.fileCID = event.params.fileCID
+  entity.numberOfQuestions = event.params.numberOfQuestions
+  entity.rewardByAnswer = event.params.rewardByAnswer
+  entity.endTimestamp = event.params.endTimestamp
+  entity.zkSources = event.params.zkSources
+  entity.questionZkSource = event.params.questionZkSource
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ test/tests/survey-contract.test.ts
@@ -0,0 +1,64 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { BigInt, Bytes } from "@graphprotocol/graph-ts"
+import { SurveyAnswered } from "../generated/schema"
+import { SurveyAnswered as SurveyAnsweredEvent } from "../generated/SurveyContract/SurveyContract"
+import { handleSurveyAnswered } from "../src/survey-contract"
+import { createSurveyAnsweredEvent } from "./survey-contract-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let fileCID = "Example string value"
+    let answers = [123]
+    let zkAnswers = [BigInt.fromI32(234)]
+    let newSurveyAnsweredEvent = createSurveyAnsweredEvent(
+      fileCID,
+      answers,
+      zkAnswers
+    )
+    handleSurveyAnswered(newSurveyAnsweredEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("SurveyAnswered created and stored", () => {
+    assert.entityCount("SurveyAnswered", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "SurveyAnswered",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "fileCID",
+      "Example string value"
+    )
+    assert.fieldEquals(
+      "SurveyAnswered",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "answers",
+      "[123]"
+    )
+    assert.fieldEquals(
+      "SurveyAnswered",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "zkAnswers",
+      "[234]"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
